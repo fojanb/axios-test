71bd469284c34a93231a2e5fc71e1aaea36c46fa
Fojan Babaali <fozhanmail@gmail.com>
2021-06-04 22:04:59 -0700

    Create Axiostest.js

---
+++ components/Axiostest.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Axiostest = () => {
+  const [info, setInfo] = useState();
+  const myRequest = async () => {
+    const res = await axios.get("https://api.github.com/users/fojanb");
+    // console.log(res);
+    console.log(
+      "Boom!Your GitHub information had been fetched successfully(^_^)"
+    );
+    setInfo(res);
+  };
+  return (
+    <div>
+      <button onClick={myRequest}>Click to get data from server</button>
+    </div>
+  );
+};
+export default Axiostest;
+
+// What axios will return ? it returns an object (json) consist of :
+// data - the payload returned from the server
+// status - the HTTP code returned from the server
+// statusText - the HTTP status message returned by the server
+// headers - headers sent by server
+// config - the original request configuration
+// request - the request object
